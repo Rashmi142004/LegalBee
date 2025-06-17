d90bf26a346cf3107c2f766b798d606cc57fb060
Rashmi142004 <rizz35436@gmail.com>
2025-06-17 23:00:23 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h2>Contact Us</h2>
+      <p>Email: <a href="mailto:rizz35436@gmail.com">rizz35436@gmail.com</a></p>
+    </div>
+  );
+};
+
+export default Contact;
