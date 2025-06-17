c6b52079a3912232a1da5ad4b8c30c14453ea21b
Rashmi142004 <rizz35436@gmail.com>
2025-06-17 22:52:55 +0530

    Create Home.js

---
+++ LegalBee/client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to LegalBee 🐝</h1>
+      <p>
+        We simplify legal information and help you understand your rights.
+        Choose a category, ask your question, or upload a legal document to get a clear explanation.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
